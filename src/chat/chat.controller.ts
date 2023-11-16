d392b287c376d0073e61ed4e1857706245075b2e
pmichalski98 <dev.michalski98@gmail.com>
2023-11-16 23:10:54 +0100

    trying to deploy

--- src/chat/chat.controller.ts
+++ src/chat/chat.controller.ts
@@ -1,9 +1,14 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { ChatService } from './chat.service';
 
 @Controller('chat')
 export class ChatController {
   constructor(private chatService: ChatService) {}
+
+  @Get()
+  test() {
+    return 'test, halo halo';
+  }
   @Post()
   async getPrompt(@Body() body: IGetPrompt) {
     const chatResponse = await this.chatService.act(body.query);
--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
     origin: 'http://localhost:3000',
     credentials: true,
   });
-  await app.listen(3001);
+  await app.listen(5000);
 }
 bootstrap();
